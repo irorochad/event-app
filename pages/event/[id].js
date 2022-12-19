4c50bbf156366ce02e291533e516e8feba07a2a0
irorochad <wisdomchadere@gmail.com>
2022-12-19 22:09:20 +0100

    re-preconfigure the projext boilerplate

---
+++ pages/event/[id].js
@@ -0,0 +1,9 @@
+const EventIdPage = () => {
+  return (
+    <div>
+      <h1>We're live here in the *ID* page...</h1>
+    </div>
+  );
+};
+
+export default EventIdPage;
---
+++ pages/event/index.js
@@ -0,0 +1,9 @@
+const EventIndex = () => {
+  return (
+    <div>
+      <h1>The event page</h1>
+    </div>
+  );
+};
+
+export default EventIndex;
