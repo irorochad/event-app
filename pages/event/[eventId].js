337f3d76bf3e5564ea0a65558f0a861dfb914cb2
irorochad <wisdomchadere@gmail.com>
2023-01-12 17:43:36 +0100

    worked on the eventId to get the ID the user will provide, plus did small styles

---
+++ pages/event/[eventId].js
@@ -0,0 +1,34 @@
+import { useRouter } from "next/router";
+
+import { getEventById } from "../../dummy-data";
+
+// Events infoo.
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventContent from "../../components/event-detail/event-content";
+
+const EventDetialPage = () => {
+  const router = useRouter();
+  const eventId = router.query.eventId;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>That Event is not available.</p>;
+  }
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.address}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
+};
+
+export default EventDetialPage;
--- pages/events/eventId.js
+++
@@ -1,9 +0,0 @@
-const EventIdPage = () => {
-  return (
-    <div>
-      <h1>We're live here in the *ID* page...</h1>
-    </div>
-  );
-};
-
-export default EventIdPage;
